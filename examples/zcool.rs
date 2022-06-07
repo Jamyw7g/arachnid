cd489ae5e20bfcae522e1d66df664eb5803dd069
wuyaoping <jamykp@gmail.com>
2022-06-07 12:24:16 +0800

    chore: perfect example

--- examples/zcool.rs
+++ examples/zcool.rs
@@ -4,16 +4,17 @@ use std::{
 };
 
 use arachnid::{
-    product::{Product, Value},
+    product::{Item, Product, Value, MidProduct},
     request::Request,
     response::Response,
-    scheduler::{Scheduler, PipeLine},
-    Selector
+    scheduler::{Scheduler, Pipeline, Middleware},
+    Selector, Easy2, List
 };
 
 fn main() -> anyhow::Result<()> {
-    let pipe: Vec<PipeLine> = vec![];
-    let mut scheduler = Scheduler::new(pipe, None)?;
+    let pipe: Vec<Pipeline> = vec![Box::new(pipe_show)];
+    let mids: Vec<Middleware> = vec![Box::new(header_mid)];
+    let mut scheduler = Scheduler::new(pipe, mids, None)?;
     for idx in 1..3 {
         let url = format!(
             "https://www.zcool.com.cn/discover?cate=33&subCate=34&page={}",
@@ -24,6 +25,21 @@ fn main() -> anyhow::Result<()> {
     }
 
     scheduler.run()
+}
+
+fn header_mid(req: Request) -> MidProduct {
+    let mut easy = Easy2::new(Response::default());
+    easy.url(&req.url).unwrap();
+    let mut headers = List::new();
+    headers.append("User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.5 Safari/605.1.15").unwrap();
+    easy.http_headers(headers).unwrap();
+
+    MidProduct::Easy((easy, req))
+}
+
+fn pipe_show(item: Item) -> Option<Item> {
+    println!("{:?}", item);
+    None
 }
 
 fn page_parse(resp: Response, tx: Arc<SyncSender<Product>>) {
